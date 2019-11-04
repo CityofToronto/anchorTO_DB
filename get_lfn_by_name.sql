5add0e2a9cfd4e4315f204e0636f69b1c05d3179
slee5toronto <slee5@toronto.ca>
2019-11-04 16:07:55 -0500

    New function to get lfn by name

---
+++ get_lfn_by_name.sql
@@ -0,0 +1,48 @@
+-- FUNCTION: code_src.get_lfn_by_name(text)
+
+-- DROP FUNCTION code_src.get_lfn_by_name(text);
+
+CREATE OR REPLACE FUNCTION code_src.get_lfn_by_name(
+	v_linear_name text)
+    RETURNS SETOF json 
+    LANGUAGE 'sql'
+
+    COST 100
+    VOLATILE SECURITY DEFINER 
+    ROWS 1000
+AS $BODY$
+/*
+  Summary:
+    Get LFN by name
+  Testing:
+    SELECT get_lfn_by_name('dyas')
+*/
+   SELECT row_to_json(c) 
+   FROM
+   (	 
+	   SELECT * 
+	   FROM 
+	   (
+			SELECT format_string(UPPER(n.name_part || ' ' || coalesce(n.type_part,'') || ' ' || coalesce(n.dir_part,''))) AS full_name, 
+				   n.objectid,
+				   n.linear_name_id,
+				   n.name_part,
+				   n.dir_part,
+				   n.type_part
+			FROM linear_name_evw n
+			WHERE format_string(UPPER(n.name_part || coalesce(n.type_part,'') || coalesce(n.dir_part,''))) LIKE UPPER(v_linear_name || '%')
+			  AND n.trans_id_expire = -1
+		) t
+		ORDER BY 1
+    ) c	
+	;	
+   $BODY$;
+
+ALTER FUNCTION code_src.get_lfn_by_name(text)
+    OWNER TO network;
+
+GRANT EXECUTE ON FUNCTION code_src.get_lfn_by_name(text) TO anchorto_run;
+
+GRANT EXECUTE ON FUNCTION code_src.get_lfn_by_name(text) TO network;
+
+REVOKE ALL ON FUNCTION code_src.get_lfn_by_name(text) FROM PUBLIC;
